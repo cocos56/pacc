e07bdaafeae24a5101d2f8212a8f97b70aca6b85
cocos56 <zj175@139.com>
2022-02-06 19:53:23 +0800

    更新于2022年02月06日	19:53:20.22

--- pacc/noxProj/hxccm.py
+++ pacc/noxProj/hxccm.py
@@ -16,7 +16,7 @@ class Activity:
 
 
 class HXCCM(NoxProj):
-    def __init__(self, startIndex, iCode='F3GWZN', noxWorkPath=r'D:\Program Files\Nox\bin', noxStep=3):
+    def __init__(self, startIndex=0, iCode='F3GWZN', noxWorkPath=r'D:\Program Files\Nox\bin', noxStep=3):
         self.startIndex = startIndex
         self.iCode = iCode
         super(HXCCM, self).__init__(noxWorkPath)
