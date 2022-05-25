a27f8b2704e88606a9e5bb910a65bd3f0778c06b
C1 <zj175@139.com>
2022-05-25 22:53:04 +0800

    增加调试信息

--- main_nox.py
+++ main_nox.py
@@ -1,8 +1,16 @@
 from pacc.noxProj import HXCCM
-from pacc.nox import NoxConsole
+from pacc.nox import NoxConsole, getOnlineDevices, NoxADB, NoxUIAutomator
 
+# HXCCM(1).runApp()
 # HXCCM(144).mainLoop()
 HXCCM(-1)
 # NoxConsole.remove(100)
 # NoxConsole.copy(100)
-NoxConsole.getNumber()
+# NoxConsole.getNumber()
+for i in getOnlineDevices()[0:1]:
+    uiaIns = NoxUIAutomator(i)
+    # NoxADB(i).getCurrentFocus()
+    # uiaIns.getScreen()
+    # uiaIns.getCurrentUIHierarchy()
+    dic = uiaIns.getDict(contentDesc='跳过')
+    print(dic)
