1181b4570f176e5674179d9531036f49a37d1485
C1 <zj175@139.com>
2022-06-19 21:57:44 +0800

    优化程序入口

--- main_hxc.py
+++ main_hxc.py
@@ -2,8 +2,14 @@ from pacc.noxProj import HXCCM
 from pacc.nox import NoxConsole
 from random import randint
 
-HXCCM(0, 'Q34J3G').mainLoop()
-
 HXCCM(-1)
-# NoxConsole.removeAll()
-# NoxConsole.copy(randint(140, 180))
+NoxConsole.removeAll()
+NoxConsole.copy(randint(140, 180))
+
+HXCCM(0, '6BIJ4R').mainLoop()
+# HXCCM(0, 'XP2971').mainLoop()
+
+
+# HXCCM(0, 'EL9IQV').mainLoop()
+# HXCCM(0, 'Q34J3G').mainLoop()
+
