5f65ab5dcb6d89d3e7219964ce1c1f1cf136eedc
C1 <zj175@139.com>
2022-06-20 07:14:55 +0800

    删除无用的空行，优化导入

--- main_hxc.py
+++ main_hxc.py
@@ -1,9 +1,10 @@
 """
 含羞草传媒程序入口
 """
-from pacc.noxProj import HXCCM
-from pacc.nox import NoxConsole
 from random import randint
+
+from pacc.nox import NoxConsole
+from pacc.noxProj import HXCCM
 
 HXCCM(-1)
 NoxConsole.removeAll()
@@ -12,7 +13,5 @@ NoxConsole.copy(randint(140, 180))
 # HXCCM(0, '6BIJ4R').mainLoop()
 HXCCM(0, 'XP2971').mainLoop()
 
-
 # HXCCM(0, 'EL9IQV').mainLoop()
 # HXCCM(0, 'Q34J3G').mainLoop()
-
