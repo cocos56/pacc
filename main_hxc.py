5cd05a134b595e36c34b3f1f57d0c1ada122db52
C1 <zj175@139.com>
2022-06-21 12:55:15 +0800

    优化含羞草程序入口模块

--- main_hxc.py
+++ main_hxc.py
@@ -6,11 +6,11 @@ from pacc.nox import NoxConsole
 from pacc.noxProj import HXCCM
 
 HXCCM(-1)
-NoxConsole.remove_all()
-NoxConsole.copy(randint(140, 180))
+# NoxConsole.remove_all()
+# NoxConsole.copy(randint(140, 180))
 
-HXCCM(0, 'EL9IQV').mainLoop()
+HXCCM(0, 'EL9IQV').mainloop()
+# HXCCM(0, 'Q34J3G').mainloop()
 
-# HXCCM(0, '6BIJ4R').mainLoop()
-# HXCCM(0, 'XP2971').mainLoop()
-# HXCCM(0, 'Q34J3G').mainLoop()
+# HXCCM(0, '6BIJ4R').mainloop()
+# HXCCM(0, 'XP2971').mainloop()
