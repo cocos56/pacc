2ee8f55f01f344381248416889009b4597c34679
C1 <zj175@139.com>
2022-06-23 14:21:38 +0800

    修复Bug

--- main_hxc.py
+++ main_hxc.py
@@ -6,10 +6,12 @@ from pacc.nox import NoxConsole
 from pacc.noxProj import HXCCM
 
 HXCCM(-1)
-NoxConsole.remove_all()
-NoxConsole.copy(randint(160, 200))
+# NoxConsole.remove_all()
+# NoxConsole.copy(randint(160, 200))
 
 HXCCM(0, '8FWWAN').mainloop()
+# HXCCM(0, 'SAP9IC').mainloop()
+
 # HXCCM(0, 'Q34J3G').mainloop()
 # HXCCM(0, 'XP2971').mainloop()
 
