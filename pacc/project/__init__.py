866d70e2115a25b9ac1129a6b4d815857991b5fc
C2 <zj175@139.com>
2022-07-11 19:53:10 +0800

    优化ADB工程包的初始化模块

--- pacc/project/__init__.py
+++ pacc/project/__init__.py
@@ -1,24 +1,6 @@
 """ADB工程包的初始化模块"""
-from .dyfd import DYFD
-from .dyjsb import DYJSB
-from .hzdyx import HZDYX
-from .iq import IQ
-from .ksjsb.ksjsb import KSJSB
-from .pdd import PDD
-from .pddsjb import PDDSJB
-from .qq import QQ
 from .sd import SD
-from .tlj import TLJ
 
 __all__ = [
-    'DYFD',
-    'DYJSB',
-    'HZDYX',
-    'IQ',
-    "KSJSB",
-    'PDD',
-    'PDDSJB',
-    'QQ',
     'SD',
-    'TLJ',
 ]
