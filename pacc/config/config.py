d478e4556e9d9b88302d310e66ac85d43f27d971
C2 <zj175@139.com>
2022-07-12 22:36:55 +0800

    屏蔽too-few-public-methods报错

--- pacc/config/config.py
+++ pacc/config/config.py
@@ -1,10 +1,15 @@
 """配置模块"""
 
 
+# pylint: disable=too-few-public-methods
 class Config:
     """配置类"""
     debug = False
 
     @classmethod
     def set_debug(cls, debug):
+        """设置是否为调试状态
+
+        :param debug: 调试状态标志
+        """
         cls.debug = debug
