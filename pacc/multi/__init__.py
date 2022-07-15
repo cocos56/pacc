8cccb2ece379bcfbf9e3bd0c2908d71f2dd14cc3
C2 <zj175@139.com>
2022-07-15 08:46:24 +0800

    修复bad-indentation错误

--- pacc/multi/__init__.py
+++ pacc/multi/__init__.py
@@ -1,3 +1,4 @@
+"""多路并发的初始化模块"""
 from .thread import runThreadsWithArgsList, runThreadsWithFunctions, threadLock
 
 __all__ = [
--- pacc/multi/thread/__init__.py
+++ pacc/multi/thread/__init__.py
@@ -1,3 +1,4 @@
+"""多线程包的初始化模块"""
 from .thread import runThreadsWithArgsList, runThreadsWithFunctions, threadLock, Thread
 
 __all__ = [
