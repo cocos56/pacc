94a989aeb29031377f449cbaa9e52b9cde9186de
C2 <zj175@139.com>
2022-07-16 11:43:19 +0800

    优化数据库增模块架构

--- pacc/mysql/create.py
+++ pacc/mysql/create.py
@@ -3,11 +3,17 @@ from .mysql import M
 
 
 class Create:
-    def __init__(self, SN):
-        self.SN = SN
+    """增类：往数据库中新增数据"""
 
     @classmethod
     def query(cls, table, fields, values):
+        """查询函数：新增数据
+
+        :param table: 表名
+        :param fields: 字段名
+        :param values: 值
+        :return: 查询到的结果（单条）
+        """
         cmd = 'insert into `%s` %s values %s' % (table, str(fields).replace("'", '`'), str(values))
         print(cmd)
         res = M.query(cmd)
@@ -16,11 +22,17 @@ class Create:
 
 
 class CreateKSJSB(Create):
-    def __init__(self, SN):
-        super(CreateKSJSB, self).__init__(SN)
+    """增类：往快手极速版数据库中新增数据"""
+    def __init__(self, sn):
+        """构造函数：初始化增类的对象
+
+        :param sn: 设备序列号
+        """
         if self.exist:
             return
-        self.query('KSJSB', ('SN', 'goldCoins', 'cashCoupons'), (SN, 0, 0))
+        self.query('KSJSB', ('SN', 'goldCoins', 'cashCoupons'), (sn, 0, 0))
 
     @property
-    def exist(self): return M.query('select 1 from `KSJSB` where `SN` = %s limit 1' % self.SN) == (1,)
+    def exist(self):
+        """创建只读属性exist，该属性用于判断是否存在快手极速版数据库中存在指定设备的数据"""
+        return M.query('select 1 from `KSJSB` where `SN` = %s limit 1' % self.sn) == (1,)
