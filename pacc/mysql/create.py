24fe89a42a4b47f2b53bad02d7fc9f0008fc2760
C2 <zj175@139.com>
2022-07-16 11:56:44 +0800

    优化字符串格式

--- pacc/mysql/create.py
+++ pacc/mysql/create.py
@@ -2,6 +2,7 @@
 from .mysql import M
 
 
+# pylint: disable=too-few-public-methods
 class Create:
     """增类：往数据库中新增数据"""
 
@@ -14,7 +15,7 @@ class Create:
         :param values: 值
         :return: 查询到的结果（单条）
         """
-        cmd = 'insert into `%s` %s values %s' % (table, str(fields).replace("'", '`'), str(values))
+        cmd = f'insert into `{table}` %s values {str(values)}' % str(fields).replace("'", '`')
         print(cmd)
         res = M.query(cmd)
         M.commit()
@@ -23,16 +24,16 @@ class Create:
 
 class CreateKSJSB(Create):
     """增类：往快手极速版数据库中新增数据"""
-    def __init__(self, sn):
+    def __init__(self, device_sn):
         """构造函数：初始化增类的对象
 
-        :param sn: 设备序列号
+        :param device_sn: 设备序列号
         """
         if self.exist:
             return
-        self.query('KSJSB', ('SN', 'goldCoins', 'cashCoupons'), (sn, 0, 0))
+        self.query('KSJSB', ('SN', 'goldCoins', 'cashCoupons'), (device_sn, 0, 0))
 
     @property
     def exist(self):
         """创建只读属性exist，该属性用于判断是否存在快手极速版数据库中存在指定设备的数据"""
-        return M.query('select 1 from `KSJSB` where `SN` = %s limit 1' % self.sn) == (1,)
+        return M.query(f'select 1 from `KSJSB` where `SN` = {self.device_sn} limit 1') == (1,)
