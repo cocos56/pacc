a43f9819a323da9e2d8fa5c952503775faf1e360
C2 <zj175@139.com>
2022-07-17 21:25:44 +0800

    优化pacc/tools/file.py模块

--- pacc/tools/captcha.py
+++ pacc/tools/captcha.py
@@ -20,10 +20,10 @@ class SliderCaptcha:
         """
         # 1. 图片剪切
         cropped_img = cv2.imread(png_file)[622:914, 166:914]  # 裁剪坐标为[y0:y1, x0:x1]
-        cv2.imwrite(File(png_file).dirPathAndFName + '_cropped.png', cropped_img)
+        cv2.imwrite(File(png_file).dir_path_and_file_name + '_cropped.png', cropped_img)
         # 2. 阴影提取
         shadow_img = np.where(np.sum(cropped_img, axis=2) < 250, np.std(cropped_img), 255)
-        shadow_png = File(png_file).dirPathAndFName + '_shadow.png'
+        shadow_png = File(png_file).dir_path_and_file_name + '_shadow.png'
         cv2.imwrite(shadow_png, shadow_img.astype(np.uint8))
         # 3. 灰色部分提取
         image = cv2.imread(shadow_png)
--- pacc/tools/file.py
+++ pacc/tools/file.py
@@ -2,15 +2,21 @@
 import os
 
 
+# pylint: disable=too-few-public-methods
 class File:
-    def __init__(self, filePath):
-        fileAbsPath = os.path.abspath(filePath)
-        dirPath, filePath = os.path.split(fileAbsPath)
+    """文件类"""
+    def __init__(self, path):
+        """构造函数
+
+        :param path: 文件的路径
+        """
+        abspath = os.path.abspath(path)
+        dir_path, file_path = os.path.split(abspath)
         # D:\0\Desktop\度盘\2020年平顶山学院暑期实训《恶魔射手》
         # 9.9.注册页面搭建(Av711127967,P9).mp4
-        fName, fExName = os.path.splitext(filePath)
+        f_name, f_ext_name = os.path.splitext(file_path)
         # 9.9.注册页面搭建(Av711127967,P9)
         # .mp4
-        self.dirPathAndFName, fExName = os.path.splitext(fileAbsPath)
+        self.dir_path_and_file_name, file_ext_name = os.path.splitext(abspath)
         # D:\0\Desktop\度盘\2020年平顶山学院暑期实训《恶魔射手》\9.9.注册页面搭建(Av711127967,P9)
         # .mp4
