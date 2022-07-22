abe7603ed07afb60d81c552df3b7736b6e26739f
C2 <zj175@139.com>
2022-07-22 10:20:08 +0800

    恢复对快手极速版的支持

---
+++ pacc/project/ksjsb/__init__.py
@@ -0,0 +1,6 @@
+"""快手极速版工程包的初始化模块"""
+from .ksjsb import KSJSB
+
+__all__ = [
+    'KSJSB'
+]
---
+++ pacc/project/ksjsb/resourceID.py
@@ -0,0 +1,26 @@
+root = 'com.kuaishou.nebula:id/'
+
+award_title_prefix = root + 'award_title_prefix'  # 观看精彩直播，可领
+award_video_close_dialog_abandon_button = root + 'award_video_close_dialog_abandon_button'  # 放弃奖励
+button2 = 'android:id/button2'  # 等待按钮（应用长时间无反应）
+animated_image = root + 'animated_image'  # 主界面左上方关闭奥运福娃按钮
+choose_tv = root + 'choose_tv'  # 请选择你要进行的操作（不感兴趣、关注）
+close = root + 'close'  # 关闭邀请新朋友（面对面邀请一个朋友赚51元）
+comment_header_close = root + 'comment_header_close'  # 关闭评论
+cycle_progress = root + 'cycle_progress'  # 金币进度
+dialog_close = root + 'dialog_close'  # 关闭电商购物优惠券
+description = root + 'description'  # 网络连接失败，请稍后重试
+left_btn = root + 'left_btn'  # 主界面左上角菜单项
+negative = 'com.kuaishou.nebula:id/negative'  # 忽略（打开推送通知，对方有更新时，第一时间获得提醒。）
+nick = root + 'nick'  # 看直播页面主播昵称
+red_packet_anim = root + 'red_packet_anim'  # 主界面右上方红包图标
+gold_egg_anim = root + 'gold_egg_anim'  # 金蛋
+iv_close_common_dialog = root + 'iv_close_common_dialog'  # 主界面右上方关闭奥运夺冠瞬间界面
+positive = root + 'positive'  # 主界面中间青少年模式，我知道了
+tab_text = root + 'tab_text'  # 详细信息/评论
+live_follow_guide_card_button = root + 'live_follow_guide_card_button'  # 立即关注（财富界面进入）
+live_exit_button = root + 'live_exit_button'  # 直接退出（直播）
+exit_btn = root + 'exit_btn'  # 退出（直播）
+live_simple_play_swipe_text = root + 'live_simple_play_swipe_text'  # 点击进入直播间
+open_long_atlas = root + 'open_long_atlas'  # 点击打开长图
+tv_upgrade_now = root + 'tv_upgrade_now'  # 免打扰升级
