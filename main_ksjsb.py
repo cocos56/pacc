936acbdd1a10c187493e45188ffb2d0a39605147
C2 <zj175@139.com>
2022-07-29 19:50:50 +0800

    新增WebSocket服务器和客户端模块

--- main_ksjsb.py
+++ main_ksjsb.py
@@ -4,10 +4,10 @@ from pacc.config import Config
 
 
 Config.set_debug(True)
-# KSJSB('003001002').mainloop()
+KSJSB('003001002').mainloop()
 # KSJSB('003001002').open_app()
 # KSJSB('003001002').open_treasure_box()
-KSJSB('003001002').change_money()
+# KSJSB('003001002').change_money()
 # KSJSB('003001002').enter_wealth_interface()
 # KSJSB('003001002').update_wealth()
 # KSJSB('003001002').get_wealth()
---
+++ pacc/network/server.py
@@ -0,0 +1,28 @@
+from websocket_server import WebsocketServer
+
+
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+    print(f"New client connected and was given id {client['id']}")
+    server.send_message_to_all("Hey all, a new client has joined us")
+
+
+# Called for every client disconnecting
+def client_left(client, server):
+    print(f"Client{client['id']} disconnected")
+
+
+# Called when a client sends a message
+def message_received(client, server, message):
+    if len(message) > 200:
+        message = message[:200] + '..'
+    print(f"Client({client['id']}) said: {message}")
+
+
+if __name__ == "__main__":
+    ws_server = WebsocketServer('0.0.0.0', 56)
+    ws_server.set_fn_new_client(new_client)
+    ws_server.set_fn_client_left(client_left)
+    ws_server.set_fn_message_received(message_received)
+    print("启动成功")
+    ws_server.run_forever()
