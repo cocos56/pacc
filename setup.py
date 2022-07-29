7d47c4b494ec80727d36e9d3b7b29701b9c23c18
C2 <zj175@139.com>
2022-07-29 19:59:00 +0800

    更新于2022年07月29日	19:59:00.70

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pacc',  # 包名
-    version='0.0.371',  # 版本
+    version='0.0.372',  # 版本
     packages=find_packages(),  # 目录下所有文件
     description="""Python Android Cluster Control 为在Windows上使用Python通过USB（通用串行总线）或
                     WLAN（基于IPv4的无线局域网）对Android手机集群（包含模拟器，目前已适配夜神模拟器）的控制提供支持""",  # 描述信息
@@ -12,23 +12,24 @@ setup(
     目前最新版已集成的功能如下：\n
     1. ADB（安卓调试桥）及UIAutomator（用户界面自动化器）\n
     2. MySQL数据库\n
-    3. 夜神模拟器（Nox.exe、NoxConsole.exe、nox_adb.exe）\n
+    待集成的功能如下：\n
+    1. 夜神模拟器（Nox.exe、NoxConsole.exe、nox_adb.exe）\n
     已实现的中央监控系统如下：\n
     1. 淘宝/拼多多全自动远程刷单APP中央控制系统\n
-    2. HXCCM拉新（全自动拉人头）中央控制系统\n
-    3. 汉字大英雄全自动看广告脚本app中央控制系统\n
     待实现的中央监控系统如下：\n
     1. 欢友全自动聊天、评论、接打视频脚本app中央监控系统\n
-    已实现的中央控制脚本如下：\n
+    2. 含羞草传媒拉新（全自动拉人头）中央控制系统\n
+    3. 汉字大英雄全自动看广告脚本app中央控制系统\n
+    正在实现中的中央控制脚本如下：\n
     1. 快手极速版全自动刷金币（刷视频、刷广告、刷直播）中央控制脚本\n
-    2. 抖音极速版全自动刷金币中央控制脚本\n
-    3. 拼多多批发版一折商品全自动采集并发送至QQ群的中央控制脚本\n
     待实现的中央控制脚本如下：\n
     1. 点淘全自动刷金币中央控制脚本\n
     2. 抖音全自动抢福袋脚本app中央控制脚本\n
     3. 淘礼金全自动抢单（淘宝0元购）中央控制脚本\n
     4. 他趣全自动拉黑女粉、聊天、接视频中央控制脚本\n
     5. 拼多多自动连接人工客服\n
+    6. 抖音极速版全自动刷金币中央控制脚本\n
+    7. 拼多多批发版一折商品全自动采集并发送至QQ群的中央控制脚本\n
     源码可通过如下渠道获取：\n
     Gitee：https://gitee.com/coco56/pacc\n
     Github：https://github.com/cocos56/pacc\n
