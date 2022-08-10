5b49886763eab34c0339782e8d9fb8b762f8bda1
C2 <zj175@139.com>
2022-08-10 11:54:29 +0800

    优化模块命名

---
+++ m_ucc_c.py
@@ -0,0 +1,4 @@
+"""统一计算中心（Unified Computing Center, UCC）客户端程序入口"""
+from pacc.network import Client
+
+Client()
---
+++ m_ucc_s.py
@@ -0,0 +1,4 @@
+"""统一计算中心（Unified Computing Center, UCC）服务器端程序入口"""
+from pacc.network import Server
+
+Server()
