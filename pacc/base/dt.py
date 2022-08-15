73f4e94df6f206a44bae6bb003709e5efe6457f8
C2 <zj175@139.com>
2022-08-16 00:39:51 +0800

    更新于2022年08月16日	 0:39:51.78

--- pacc/base/dt.py
+++ pacc/base/dt.py
@@ -20,5 +20,5 @@ def show_datetime(text: str, language=Language.CH):
     if language == Language.CH:
         print(f"\n现在是：{datetime.now()}，正在执行{text}，已运行{Datetime.get_run_time()}")
     elif language == Language.EN:
-        print(f"\nNow is: {datetime.now()}, executing {text}, it has been running for "
-              f"{Datetime.get_run_time()}")
+        print(f"Now is: {datetime.now()}, executing {text}, it has been running for "
+              f"{Datetime.get_run_time()}\n")
