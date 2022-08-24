a990a9607e1dbcdb91d000ddb31e32f4e3037440
C2 <zj175@139.com>
2022-08-25 06:53:24 +0800

    更新于2022年08月25日	 6:53:24.63

--- mains/m_ks23.py
+++ mains/m_ks23.py
@@ -3,6 +3,6 @@ from pacc.config import Config
 from pacc.project import Ksjsb
 
 Config.set_debug(True)
-Ksjsb('002002003').mainloop()
+# Ksjsb('002002003').mainloop()
 # Ksjsb('002002003').get_daily_challenge_coins(True, False)
-Ksjsb('002002003').mainloop()
+Ksjsb('002002003').get_double_bonus(False)
--- pacc/pacc.py
+++ pacc/pacc.py
@@ -3,6 +3,6 @@
 
 def get_version():
     """获取当前PACC的版本号"""
-    version = '0.0.414'
+    version = '0.0.415'
     print(f'Your PACC version is {version}')
     return version
