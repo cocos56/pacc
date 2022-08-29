552e02460b1ae96c6104dcd64f983ba20889cbee
C2 <zj175@139.com>
2022-08-29 09:35:37 +0800

    优化去逛街方法

--- pacc/project/ksjsb/ksjsb.py
+++ pacc/project/ksjsb/ksjsb.py
@@ -362,7 +362,7 @@ class Ksjsb(Project):
                 sleep(1)
                 countdown -= 1
                 print(countdown)
-                self.adb_ins.swipe(536, 1600, 536, 500, 2000)
+                self.adb_ins.swipe(536, 1100, 536, 1000, 2000)
                 current_focus = self.adb_ins.get_current_focus()
                 if Activity.KwaiYodaWebViewActivity in current_focus:
                     break_while = True
