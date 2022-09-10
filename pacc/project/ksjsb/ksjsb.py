37a5918ae710405bb6fece8d5825c2b333a7296d
C2 <zj175@139.com>
2022-09-10 19:32:07 +0800

    更新于2022年09月10日	19:32:07.38

--- pacc/project/ksjsb/ksjsb.py
+++ pacc/project/ksjsb/ksjsb.py
@@ -566,7 +566,7 @@ class Ksjsb(Project):
                 break
         self.uia_ins.click(text='立即兑换', xml=self.uia_ins.xml)
         sleep(3)
-        if self.uia_ins.click_by_screen_text('极速到账'):
+        if self.uia_ins.click_by_screen_text('极速到账', start_index=1):
             if Activity.ResolverActivity in self.adb_ins.get_current_focus():
                 self.uia_ins.click(ResourceID.button2)
             sleep(50)
