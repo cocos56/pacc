52ac4daa059130862d16b1d531fff36c850c231d
C2 <zj175@139.com>
2022-09-22 14:54:23 +0800

    修复获取金币购划算页面内的所有奖励的方法中存在的逻辑错误

--- m.py
+++ m.py
@@ -14,13 +14,17 @@ adb_ins = ADB(DEVICE_SN)
 # print(adb_ins.get_battery_temperature())
 # adb_ins.get_current_focus()
 uia_ins = UIAutomator(DEVICE_SN)
+info = uia_ins.get_dict('app')['node'][2]['node'][2]['node'][0]['node']
+i1 = info[3]['@text']
+print(i1)
+print(info[4]['@text'][1:-1])
 # print(uia_ins.get_dict('withdrawDialog')['node']['node']['node'][3]['@text'])
 # print(adb_ins.is_awake())
 # uia_ins.click('com.jifen.qukan:id/a8w')
 # uia_ins.click(naf='true', index='1', start_index=2)
 # print(uia_ins.get_dict('android:id/button2', '等待'))
 # uia_ins.click_by_screen_text('关闭')
-try:
-    uia_ins.get_current_ui_hierarchy()
-except FileNotFoundError as err:
-    print_err(err)
+# try:
+#     uia_ins.get_current_ui_hierarchy()
+# except FileNotFoundError as err:
+#     print_err(err)
--- pacc/project/ksjsb/ksjsb.py
+++ pacc/project/ksjsb/ksjsb.py
@@ -520,19 +520,16 @@ class Ksjsb(Project):
             if click_cnt >= 10:
                 print('在获取金币购划算页面内的领福利奖励过程中已经累计领了10次福利，无需继续')
                 break
-        click_cnt = 0
-        while self.uia_ins.click_by_screen_text(text='去完成', txt=self.uia_ins.txt):  # 已完成
-            sleep(6)
-            if Activity.KwaiYodaWebViewActivity in self.adb_ins.get_current_focus():
-                self.uia_ins.tap((240, 848), 96)
-                self.exit_live(Activity.KwaiYodaWebViewActivity)
-                self.adb_ins.press_back_key(9)
+        if self.uia_ins.click_by_screen_text(text='去完成', txt=self.uia_ins.txt):  # 已完成
             self.uia_ins.txt = ''
-            click_cnt += 1
-            print(f'buy_things_with_coins 去完成 click_cnt={click_cnt}')
-            if click_cnt >= 8:
-                print('在获取金币购划算页面内的去完成过程中已经累计去完成了8次，无需继续')
-                break
+            while True:
+                self.adb_ins.swipe((600, 1833), (600, 560))
+                if Activity.KwaiYodaWebViewActivity in self.adb_ins.get_current_focus():
+                    self.uia_ins.tap((240, 848), 39)
+                    self.exit_live(Activity.KwaiYodaWebViewActivity)
+                info = self.uia_ins.get_dict('app')['node'][2]['node'][2]['node'][0]['node']
+                if int(info[3]['@text']) == int(info[4]['@text'][1:-1]):
+                    break
         click_cnt = 0
         while self.uia_ins.click_by_screen_text('去逛街', txt=self.uia_ins.txt):  # 已领取
             countdown = 96
