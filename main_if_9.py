0c6413a2e6fa9dcffbda983f91d064d341c5c59b
C3 <zj175@139.com>
2022-10-24 09:03:34 +0800

    增加对雷电模拟器9的支持

---
+++ main_if_9.py
@@ -0,0 +1,7 @@
+"""雷电9_咸鱼程序入口模块"""
+from pacc.ld_proj.idle_fish import IdleFish
+from pacc.adb.ld_console import LDConsole
+
+IdleFish(0, r'F:\leidian\LDPlayer9')
+LDConsole.quit_all()
+IdleFish.mainloop(1, 2)
--- pacc/ld_proj/idle_fish.py
+++ pacc/ld_proj/idle_fish.py
@@ -16,12 +16,13 @@ class Activity:  # pylint: disable=too-few-public-methods
 class IdleFish(LDProj):
     """咸鱼类"""
 
-    def __init__(self, ld_index=1):
+    def __init__(self, ld_index=1, ld_work_path=r'F:\leidian\LDPlayer4'):
         """构造函数
 
         :param ld_index: 目标雷电模拟器的索引值
+        :param ld_work_path: 雷电模拟器的工作路径
         """
-        super().__init__()
+        super().__init__(ld_work_path)
         self.ld_index = ld_index
 
     def run_app(self):
