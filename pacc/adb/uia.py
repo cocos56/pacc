14054abd9f99114f5a4f336f0168767963483c00
C2 <zj175@139.com>
2022-10-25 08:38:11 +0800

    在pacc.adb.uia模块中增加屏蔽R0801: Similar lines in 2 files报警

--- pacc/adb/uia.py
+++ pacc/adb/uia.py
@@ -468,7 +468,7 @@ class UIAutomator:
                     self.dicts.append(res)
         return False
 
-    def get_current_ui_hierarchy(self):
+    def get_current_ui_hierarchy(self):  # pylint: disable=R0801
         """获取当前用户界面上元素的层次布局信息
 
         :return: 正常情况下会返回当前的用户界面上的元素的层次布局信息所构成的xml字符串，如果遇到异常则不做处理直接传递
