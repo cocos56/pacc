51246e655609d4564b9876f6d5c865d542a9e3d8
C3 <zj175@139.com>
2022-11-21 12:33:35 +0800

    修复每日薅羊毛任务不做的情况下造成的阻塞问题：奖励：闲鱼币x

--- pacc/ld_proj/idle_fish.py
+++ pacc/ld_proj/idle_fish.py
@@ -122,11 +122,19 @@ class IdleFish(LDProj):
             cls(index).run_app()
             return cls.check_target_device(index)
         dic = lduia_ins.get_dict(content_desc='我的经验', xml=lduia_ins.xml)
-        ex_p = int(dic['@content-desc'][5:])
+        try:
+            ex_p = int(dic['@content-desc'][5:])
+        except TypeError as err:
+            print_err(err)
+            cls(index).run_app(30)
+            return cls.check_target_device(index)
         for _ in range(int(ex_p/200)):
             lduia_ins.tap((276, 600))
-        if ex_p >= 200 and lduia_ins.get_dict(content_desc='领取闲鱼币，去开新店'):
-            lduia_ins.tap((283, 763), 3)
+        if ex_p >= 200:
+            if lduia_ins.get_dict(content_desc='领取闲鱼币，去开新店'):
+                lduia_ins.tap((283, 763), 3)
+            elif lduia_ins.get_dict(content_desc='奖励：闲鱼币x', xml=lduia_ins.xml):
+                lduia_ins.tap((264, 709), 3)
             return cls.check_target_device(index)
         if lduia_ins.get_dict(content_desc='点击领取', xml=lduia_ins.xml):
             lduia_ins.tap((453, 492), 3)
