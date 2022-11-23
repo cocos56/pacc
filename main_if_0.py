325f6a423651b3df619d3f9b9b67689dc2d7b98b
C3 <zj175@139.com>
2022-11-23 23:53:25 +0800

    扩展终止索引值到397

--- main_if_0.py
+++ main_if_0.py
@@ -4,7 +4,7 @@ from pacc.ld_proj.idle_fish import IdleFish
 
 Config.debug = True
 Config.set_ld_work_path()
-start_index, end_index = 1, 392
+start_index, end_index = 1, 397
 # IdleFish.check_version(start_index, end_index)
-IdleFish.check(start_index, end_index, 3)
-# IdleFish.mainloop(start_index, end_index, 3)
+# IdleFish.check(start_index, end_index, 3)
+IdleFish.mainloop(start_index, end_index, 3)
