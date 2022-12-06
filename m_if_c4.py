4251bcfaadbcb444f66f40126338327e2dda38d4
C4 <zj175@139.com>
2022-12-06 10:43:53 +0800

    扩展C4的终止索引值至48

--- m_if_c4.py
+++ m_if_c4.py
@@ -4,7 +4,7 @@ from pacc.ld_proj.idle_fish import IdleFish
 
 Config.debug = True
 Config.set_ld_work_path()
-start_index, end_index = 1, 40
+start_index, end_index = 1, 48
 # IdleFish.check_version(start_index, end_index)
-# IdleFish.check(10, end_index)
+# IdleFish.check(start_index, end_index)
 IdleFish.mainloop(start_index, end_index, 5)
