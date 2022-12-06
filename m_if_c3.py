3e8cb16834367a312b28d085af8eefbbb5a286ee
C3 <zj175@139.com>
2022-12-06 08:19:30 +0800

    优化主循环中判断的时间点

--- m_if_c3.py
+++ m_if_c3.py
@@ -4,7 +4,7 @@ from pacc.ld_proj.idle_fish import IdleFish
 
 Config.debug = True
 Config.set_ld_work_path()
-start_index, end_index = 1, 405
+start_index, end_index = 1, 410
 # IdleFish.check_version(start_index, end_index)
 # IdleFish.check(41, end_index)
 IdleFish.mainloop(start_index, end_index, 5)
