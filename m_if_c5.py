a6ace82356ec5aa749a5085f90d0c3119069a3e7
C5 <zj175@139.com>
2022-12-18 19:52:32 +0800

    增加对C5的支持

---
+++ m_if_c5.py
@@ -0,0 +1,11 @@
+"""雷电9_咸鱼程序入口模块"""
+from pacc.config import Config
+from pacc.ld_proj.idle_fish import IdleFish
+
+Config.debug = True
+Config.set_ld_work_path()
+start_index, end_index = 1, 1
+# IdleFish.check_version(start_index, end_index)
+# IdleFish.check(159, end_index)
+# IdleFish.check_odd_devices(101, end_index)
+IdleFish.mainloop(start_index, end_index, 5)
---
+++ m_if_c5_2.py
@@ -0,0 +1,11 @@
+"""雷电9_咸鱼程序入口模块"""
+from pacc.config import Config
+from pacc.ld_proj.idle_fish import IdleFish
+
+Config.debug = True
+Config.set_ld_work_path()
+start_index, end_index = 1, 1
+# IdleFish.backups(start_index, end_index)
+IdleFish.check_version(start_index, end_index)
+# IdleFish.check(start_index, end_index, 5)
+# IdleFish.check_even_devices(101, end_index)
