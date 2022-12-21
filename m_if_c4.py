cb188a2f50ade3b26fa993dad09edd44da452245
C4 <zj175@139.com>
2022-12-21 18:15:17 +0800

    扩展C4的终止索引值至176

--- m_if_c4.py
+++ m_if_c4.py
@@ -4,7 +4,7 @@ from pacc.ld_proj.idle_fish import IdleFish
 
 Config.debug = True
 Config.set_ld_work_path()
-start_index, end_index = 1, 168
+start_index, end_index = 1, 176
 # IdleFish.check_version(start_index, end_index)
 # IdleFish.check(160, end_index)
 # IdleFish.check_odd_devices(101, end_index)
