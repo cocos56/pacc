d9636ac50d55ea2432ab12d1351c7c8e9cbcfccf
C5 <zj175@139.com>
2022-12-22 10:00:25 +0800

    屏蔽unused-import报警

--- m_if_c5_2.py
+++ m_if_c5_2.py
@@ -1,4 +1,5 @@
 """雷电9_咸鱼程序入口模块"""
+# pylint: disable=unused-import
 from pacc.config import Config
 from pacc.ld_proj.idle_fish import IdleFish
 from pacc.adb import LDConsole
@@ -6,7 +7,7 @@ from pacc.adb import LDConsole
 Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, 3
-LDConsole.quit(1, force_flag=True)
+LDConsole.quit(3)
 # IdleFish.backups(start_index, end_index)
 # IdleFish.check_version(start_index, end_index)
 # IdleFish.check(start_index, end_index, 5)
