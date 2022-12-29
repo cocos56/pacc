47f5245f55e21b74a74abf07acf34cc4e87af554
C4 <zj175@139.com>
2022-12-29 08:37:07 +0800

    屏蔽too-many-arguments报警

--- pacc/mysql/mysql.py
+++ pacc/mysql/mysql.py
@@ -85,6 +85,7 @@ Mobile()
 class Account(MySQL):
     """MySQL中名为account的数据库，该数据库用于存储网络账号相关的信息"""
 
+    # pylint: disable=too-many-arguments
     def __init__(self, host=getenv('MySQL_Host'), port=3306, database='account', user='root',
                  password=getenv('MySQL_PW'), charset='utf8'):
         """构造函数"""
