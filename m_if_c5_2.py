8fc9f10304efae934ffb63c91aea44ff0229811a
C5 <zj175@139.com>
2022-12-30 09:14:41 +0800

    调整C5的程序入口模块

--- m_if_c5_2.py
+++ m_if_c5_2.py
@@ -1,22 +1,10 @@
-"""雷电9_咸鱼程序入口模块"""
-# pylint: disable=unused-import
-from datetime import datetime
-
+"""闲鱼程序入口C5_2模块"""
 from pacc.adb import LDConsole
-from pacc.adb.ld_base import LDBase
 from pacc.config import Config
 from pacc.ld_proj.idle_fish import IdleFish
-from pacc import MySQLDump
 
 Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, LDConsole.get_last_device_num()
 print(f'start_index={start_index}, end_index={end_index}')
-# LDConsole.quit(3)
-# LDBase(3).timeout_monitoring(datetime.now())
-# IdleFish.backups(start_index, end_index)
-# MySQLDump.start()
 IdleFish.check_after_run(start_index, end_index)
-# IdleFish.check_version(start_index, end_index, 5)
-# IdleFish.check(start_index, end_index, 5)
-# IdleFish.check_even_devices(101, end_index)
--- m_if_c5_3.py
+++ m_if_c5_3.py
@@ -1,10 +1,13 @@
-"""闲鱼程序入口C5_1模块"""
+"""闲鱼程序入口C5_3模块"""
 from pacc.adb import LDConsole
 from pacc.config import Config
 from pacc.ld_proj.idle_fish import IdleFish
+from pacc import MySQLDump
 
 Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, LDConsole.get_last_device_num()
 print(f'start_index={start_index}, end_index={end_index}')
-IdleFish.mainloop(start_index, end_index, 5)
+IdleFish.backups(start_index, end_index)
+MySQLDump.start()
+# IdleFish.check_version(start_index, end_index, 5)
