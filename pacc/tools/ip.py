4627b5bd5eccea485fd4ec17e953a3ad8d700400
C5 <zj175@139.com>
2023-01-07 17:14:31 +0800

    优化获取本机公网IPv4地址的方法

--- pacc/tools/ip.py
+++ pacc/tools/ip.py
@@ -1,6 +1,8 @@
 """IP地址模块"""
 import requests
 from requests.exceptions import ReadTimeout
+from datetime import datetime
+from random import randint
 
 from ..base import print_err
 
@@ -11,11 +13,28 @@ def get_global_ipv4_addr():
     :return: 本机的公网IPv4地址，形如：120.219.74.14
     """
     try:
-        ipv4_addr = requests.get('https://checkip.amazonaws.com', timeout=9).text.strip()
+        ipv4_addr = requests.get('https://ident.me', timeout=9).text
     except ReadTimeout as err:
         print_err(err)
         return get_global_ipv4_addr()
-    # print([ipv4_addr])
+    return ipv4_addr
+
+
+def get_global_ipv4_addr_2():
+    """获取本机公网IPv4地址
+
+    :return: 本机的公网IPv4地址，形如：120.219.74.14
+    """
+    servers = ['http://icanhazip.com', 'https://checkip.amazonaws.com', 'https://ident.me']
+    random_server_index = randint(0, len(servers)-1)
+    try:
+        start_datetime = datetime.now()
+        ipv4_addr = requests.get(servers[random_server_index], timeout=9).text.strip()
+    except ReadTimeout as err:
+        print_err(err)
+        return get_global_ipv4_addr()
+    print(servers[random_server_index], [ipv4_addr])
+    print(datetime.now() - start_datetime)
     return ipv4_addr
 
 
