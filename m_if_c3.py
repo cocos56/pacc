13087f1f6ea1619d4c71995bd29223a36cddd39a
C3 <zj175@139.com>
2023-01-07 10:59:05 +0800

    增大超时中断时间至9秒

--- m_if_c3.py
+++ m_if_c3.py
@@ -8,4 +8,4 @@ Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, LDConsole.get_last_device_num()
 print(f'start_index={start_index}, end_index={end_index}')
-IdleFish.mainloop(start_index, end_index, 5)
+IdleFish.mainloop(370, end_index, 5)
