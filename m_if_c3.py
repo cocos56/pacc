73e148283ca25fa5f9f8c436bc19b574116464e3
C3 <zj175@139.com>
2023-01-09 05:32:27 +0800

    增大超时中断的阈值至30秒

--- m_if_c3.py
+++ m_if_c3.py
@@ -8,4 +8,5 @@ Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, LDConsole.get_last_device_num()
 print(f'start_index={start_index}, end_index={end_index}')
-IdleFish.mainloop(start_index, end_index, 5)
+# 2023-01-08: 1, 305
+IdleFish.mainloop(280, end_index, 5)
--- pacc/ld_proj/idle_fish.py
+++ pacc/ld_proj/idle_fish.py
@@ -428,7 +428,7 @@ class IdleFish(LDProj):
             if start_index > end_index:
                 print(f'所有共{end_index - src_start_index + 1}项已检查完毕，当前时间为：'
                       f'{datetime.now()}\n')
-                if datetime.now().hour >= 3 or all_done:
+                if datetime.now().hour >= 12 or all_done:
                     start_day = date.today() + timedelta(days=1)
                 src_start_index = start_index = 1
                 all_done = True
