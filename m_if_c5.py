6c246e8de8091cf88cc78273c0c60d0e65768e30
C5 <zj175@139.com>
2023-01-14 19:01:20 +0800

    解决unused-import报警

--- m_if_c5.py
+++ m_if_c5.py
@@ -7,5 +7,5 @@ Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, LDConsole.get_last_device_num()
 print(f'start_index={start_index}, end_index={end_index}')
-# IdleFish.mainloop(start_index, end_index, 5)
-IdleFishRunTask.mainloop(start_index, end_index, 5)
+IdleFish.mainloop(start_index, end_index, 5)
+# IdleFishRunTask.mainloop(start_index, end_index, 5)
