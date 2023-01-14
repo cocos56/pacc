06a3d5616b74b39acacdd3172526936f3cfa071d
C5 <zj175@139.com>
2023-01-14 13:56:44 +0800

    结局import-error错误

--- m_if_c5.py
+++ m_if_c5.py
@@ -1,10 +1,11 @@
 """闲鱼程序入口C5_1模块"""
 from pacc.adb import LDConsole
 from pacc.config import Config
-from pacc.ld_proj import IdleFish
+from pacc.ld_proj import IdleFish, IdleFish2
 
 Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, LDConsole.get_last_device_num()
 print(f'start_index={start_index}, end_index={end_index}')
 IdleFish.mainloop(start_index, end_index, 5)
+IdleFish2.mainloop(start_index, end_index, 5)
