b7e4e5353f582ebdee32bf9d6cee1a131514aab9
C5 <zj175@139.com>
2023-01-20 07:41:15 +0800

    解决no-name-in-module报警

--- m_if_c5.py
+++ m_if_c5.py
@@ -1,12 +1,10 @@
 """闲鱼程序入口C5_1模块"""
-# pylint: disable=unused-import
 from pacc.adb import LDConsole
 from pacc.config import Config
-from pacc.ld_proj import IdleFish, IdleFishRunTask
+from pacc.ld_proj import IdleFish
 
 Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, LDConsole.get_last_device_num()
 print(f'start_index={start_index}, end_index={end_index}')
 IdleFish.mainloop(start_index, end_index, 5)
-# IdleFishRunTask.mainloop(start_index, end_index, 5)
