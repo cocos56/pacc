c4f38d13712f4b30d456c4199cfd9f93b4bd7a00
C2 <zj175@139.com>
2023-01-22 20:27:58 +0800

    增加闲鱼中控类

--- pacc/project/idle_fish.py
+++ pacc/project/idle_fish.py
@@ -3,8 +3,8 @@ from .project import Project
 from ..base import run_forever
 
 
-class PddVideo(Project):
-    """拼多多视频类"""
+class IdleFish(Project):
+    """闲鱼中控类"""
 
     @run_forever
     def mainloop(self):
