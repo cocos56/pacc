b84bef9f4e28ad2c1a2aae98fdd9311309e66d9b
C2 <zj175@139.com>
2023-01-23 15:47:07 +0800

    修复主循环方法中存在的逻辑错误

--- pacc/project/idle_fish.py
+++ pacc/project/idle_fish.py
@@ -1,6 +1,6 @@
 """闲鱼中控模块"""
 from .project import Project
-from ..base import run_forever, sleep
+from ..base import sleep, print_err
 
 ROOT = 'com.taobao.idlefish/com.taobao.idlefish.maincontainer.activity.'
 
@@ -24,18 +24,23 @@ class IdleFish(Project):
         self.adb_ins.open_app(Activity.MainActivity)
         sleep(5)
 
-    @run_forever
     def mainloop(self):
         """主循环函数"""
-        self.open_app()
-        self.adb_ins.get_current_focus()
-        self.uia_ins.click(content_desc='我的，未选中状态')
-        self.uia_ins.click(content_desc='我卖出的')
-        self.uia_ins.click(content_desc='待付款')
-        self.uia_ins.click(content_desc='修改价格')
-        dic = self.uia_ins.get_dict(class_='android.widget.EditText')
-        src_price = float(dic['@text'])
-        if str(src_price)[-1] != '1':
+        while True:
+            self.open_app()
+            self.adb_ins.get_current_focus()
+            self.uia_ins.click(content_desc='我的，未选中状态')
+            self.uia_ins.click(content_desc='我卖出的')
+            self.uia_ins.click(content_desc='待付款')
+            self.uia_ins.click(content_desc='修改价格')
+            dic = self.uia_ins.get_dict(class_='android.widget.EditText')
+            try:
+                src_price = float(dic['@text'])
+            except TypeError as err:
+                print_err(err)
+                continue
+            if str(src_price)[-1] == '1':
+                continue
             price = src_price / 10 + 0.01
             print(price)
             self.uia_ins.click(class_='android.widget.EditText')
@@ -43,4 +48,4 @@ class IdleFish(Project):
             self.uia_ins.click(content_desc='确定修改')
             self.uia_ins.click(content_desc='确定', index='1')
             self.uia_ins.get_current_ui_hierarchy()
-        sleep(1000)
+            sleep(30)
