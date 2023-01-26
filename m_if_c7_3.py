3e306e550af0fc6b4b1d244ff95139ab7bdaaa1e
C7 <zj175@139.com>
2023-01-26 22:20:44 +0800

    为闲鱼程序入口C7_3模块增加对数据库的备份功能

--- m_if_c7_3.py
+++ m_if_c7_3.py
@@ -1,4 +1,5 @@
 """闲鱼程序入口C7_3模块"""
+from pacc import MySQLDump
 from pacc.adb import LDConsole
 from pacc.config import Config
 from pacc.ld_proj.idle_fish import IdleFish
@@ -7,6 +8,7 @@ Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, LDConsole.get_last_device_num()
 print(f'start_index={start_index}, end_index={end_index}')
+MySQLDump.start()
 IdleFish.backups(start_index, end_index, reserved_gbs=6*1024)
 IdleFish.check_version(start_index, end_index, 5)
 IdleFish.update_ip(start_index, end_index)
