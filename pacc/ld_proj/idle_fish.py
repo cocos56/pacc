af730a0afeb50ce059e377316f860237b17bb69b
C7 <zj175@139.com>
2023-02-06 07:47:48 +0800

    优化登录的方法

--- pacc/ld_proj/idle_fish.py
+++ pacc/ld_proj/idle_fish.py
@@ -168,6 +168,8 @@ class IdleFish(IdleFishBase):
             update_idle_fish_ins.update_last_login_ipv4_addr(get_global_ipv4_addr())
             update_idle_fish_ins.update_login('NULL')
             update_idle_fish_ins.update_version('NULL')
+            if retrieve_idle_fish_ins.top_up_mobile_cnt:
+                update_idle_fish_ins.update_top_up_mobile_cnt(-1)
             if Activity.WebViewActivity in ldadb_ins.get_current_focus():
                 print(f'{start_index}于{datetime.now()}需要验证码登录，请输入验证码')
                 update_idle_fish_ins.update_last_hvc_date(today)
