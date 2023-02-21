753408c3c0c32e8d3cb87cc8169c79489b29f676
C1 <zj175@139.com>
2023-02-21 18:56:42 +0800

    修复unspecified-encoding的问题

--- m_if_ui.py
+++ m_if_ui.py
@@ -66,7 +66,7 @@ class IdleFishGUI:  # pylint: disable=too-many-instance-attributes
                     dic.get('login_pw') and dic.get('pay_pw'):
                 txt_name = f'{dic.get("Job_N")}{dic.get("role")}.txt'
                 print(txt_name)
-                with open(txt_name, 'w+') as file:
+                with open(txt_name, 'w+', encoding='utf-8') as file:
                     file.write(txt_name)
                 move(txt_name, join(r'\\10.1.1.2\acs', txt_name))
                 CreateIdleFish(
