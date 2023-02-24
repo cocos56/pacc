0fd9fcb731d5019bd006183497686f962b4d61d4
C2 <zj175@139.com>
2023-02-24 11:45:51 +0800

    优化付款的方法

--- pacc/project/idle_fish.py
+++ pacc/project/idle_fish.py
@@ -100,7 +100,9 @@ class IdleFish(Project):
                     if self.uia_ins.get_dict(text='已支付', xml=self.uia_ins.xml):
                         remove(alipay_code)
                         continue
-                self.uia_ins.click(text='继续支付', interval=3)
+                if not self.uia_ins.click(text='继续支付', interval=3):
+                    remove(alipay_code)
+                    continue
                 if not self.uia_ins.click(text='确认交易'):
                     continue
             except FileNotFoundError as err:
