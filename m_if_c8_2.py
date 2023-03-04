e50a93195a86a5975a1114644befde4392b74ff8
C8 <zj175@139.com>
2023-03-04 09:24:48 +0800

    新增闲鱼程序入口C7_2模块

---
+++ m_if_c8_2.py
@@ -0,0 +1,15 @@
+"""闲鱼程序入口C7_2模块"""
+from pacc.adb import LDConsole
+from pacc.config import Config
+from pacc.ld_proj.idle_fish import IdleFish
+
+Config.debug = True
+Config.set_ld_work_path()
+start_index, end_index = 1, LDConsole.get_last_device_num()
+print(f'start_index={start_index}, end_index={end_index}')
+# IdleFish.top_up_mobile(start_index, end_index)
+# IdleFish.confirm(start_index, end_index)
+IdleFish.check_after_run(start_index, end_index)
+# IdleFish.first_buy(start_index, end_index)
+# IdleFish.create(end_index)
+# IdleFish.login(start_index, end_index)
