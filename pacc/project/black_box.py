57bbb5fa5f1aeb6a0439113c7b07d0d28e495a51
C2 <zj175@139.com>
2023-03-16 19:47:12 +0800

    新增BlackBox中控类

--- pacc/project/black_box.py
+++ pacc/project/black_box.py
@@ -1,1 +1,28 @@
 """BlackBox中控模块"""
+from .project import Project
+from ..base import sleep
+
+ROOT = 'top.niunaijun.blackboxa64/top.niunaijun.blackboxa.view.main.'
+
+
+class Activity:  # pylint: disable=too-few-public-methods
+    """BlackBox中控模块的安卓活动名类"""
+    MainActivity = f'{ROOT}MainActivity'  # 主界面
+
+
+class ResourceID:  # pylint: disable=too-few-public-methods
+    """BlackBox中控模块的安卓资源ID类"""
+    toolbar_layout = 'top.niunaijun.blackboxa64:id/toolbar_layout'
+
+
+class BlackBox(Project):
+    """BlackBox中控类"""
+
+    def open_app(self) -> None:
+        """打开BlackBox应用"""
+        self.free_memory()
+        self.adb_ins.open_app(Activity.MainActivity)
+        sleep(5)
+
+    def get_name(self):
+        """获取名字"""
