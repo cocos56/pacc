661b1a8ae02ba61af2c7131cf04d037b58d1ba82
C8 <zj175@139.com>
2023-03-28 05:37:42 +0800

    调整闲鱼程序入口C8_2模块

--- m_if_c8.py
+++ m_if_c8.py
@@ -7,7 +7,7 @@ Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, LDConsole.get_last_device_num()
 print(f'start_index={start_index}, end_index={end_index}')
-IdleFish.mainloop(start_index, end_index, 5, 130)
+IdleFish.mainloop(start_index, end_index, 5)
 # IdleFish.first_buy(start_index, end_index)
 # IdleFish.create(end_index)
 # IdleFish.login(start_index, end_index)
