00b38bd9e48523f355ad0fe0dc7c5bfe4a7f517e
C6 <zj175@139.com>
2023-04-13 09:41:08 +0800

    新增闲鱼程序入口C6_1模块

---
+++ m_if_c6.py
@@ -0,0 +1,13 @@
+"""闲鱼程序入口C6_1模块"""
+# pylint: disable=duplicate-code
+from pacc.adb import LDConsole
+from pacc.config import Config
+from pacc.ld_proj import IdleFish
+
+Config.debug = True
+Config.set_ld_work_path()
+start_index, end_index = 1, LDConsole.get_last_device_num()
+print(f'start_index={start_index}, end_index={end_index}')
+IdleFish.mainloop(start_index, end_index, 5)
+# IdleFish.create(end_index)
+# IdleFish.login(start_index, end_index)
---
+++ m_if_c6_3.py
@@ -0,0 +1,17 @@
+"""闲鱼程序入口C6_3模块"""
+# pylint: disable=duplicate-code
+from pacc import MySQLDump
+from pacc.adb import LDConsole
+from pacc.config import Config
+from pacc.ld_proj.idle_fish import IdleFish
+
+Config.debug = True
+Config.set_ld_work_path()
+start_index, end_index = 1, LDConsole.get_last_device_num()
+print(f'start_index={start_index}, end_index={end_index}')
+MySQLDump.start()
+IdleFish.backups(start_index, end_index, reserved_gbs=6*1024)
+IdleFish.check_version(start_index, end_index)
+IdleFish.update_ip(start_index, end_index)
+IdleFish.update_hosts(start_index, end_index, 'C6')
+IdleFish.record(start_index, end_index)
