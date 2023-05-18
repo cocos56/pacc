02b13f07c8c9a0781ce623f7b676afc9079cdcf1
C2 <zj175@139.com>
2023-05-18 12:35:25 +0800

    调整淘宝/拼多多全自动远程刷单程序入口C1模块

--- scrcpy/001sd/001001007sd_淮Mi4_张德任.bat
+++ scrcpy/001sd/001001007sd_淮Mi4_张德任.bat
@@ -2,4 +2,4 @@ adb reconnect offline
 echo 应用升级：20220503
 echo 淘宝消息：20220503
 echo 拼多多消息：20220503
-scrcpy -s f574c33 --window-title 001101012sd_分Mi4_张德任|
+scrcpy -s f574c33 --window-title 001001007sd_淮Mi4_张德任|
