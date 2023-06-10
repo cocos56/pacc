95c9ad944fc9bc4748a795b7fe1d32745fbd2fce
C5 <zj175@139.com>
2023-06-10 12:55:13 +0800

    新增闲鱼程序入口C13_1模块

--- m_if_c13.py
+++ m_if_c13.py
@@ -1,4 +1,4 @@
-"""闲鱼程序入口C5_1模块"""
+"""闲鱼程序入口C13_1模块"""
 # pylint: disable=duplicate-code
 from pacc.adb import LDConsole
 from pacc.config import Config
--- m_if_c13_3.py
+++ m_if_c13_3.py
@@ -1,5 +1,6 @@
-"""闲鱼程序入口C5_2模块"""
+"""闲鱼程序入口C13_3模块"""
 # pylint: disable=duplicate-code
+from pacc import MySQLDump
 from pacc.adb import LDConsole
 from pacc.config import Config
 from pacc.ld_proj.idle_fish import IdleFish
@@ -8,8 +9,11 @@ Config.debug = True
 Config.set_ld_work_path()
 start_index, end_index = 1, LDConsole.get_last_device_num()
 print(f'start_index={start_index}, end_index={end_index}')
-IdleFish.check_after_run(start_index, end_index)
-# IdleFish.auto_buy(start_index, end_index)
-# IdleFish.confirm(start_index, end_index)
 # IdleFish.create(end_index)
 # IdleFish.login(start_index, end_index)
+MySQLDump.start()
+IdleFish.backups(start_index, end_index, reserved_gbs=6*1024)
+IdleFish.check_version(start_index, end_index)
+IdleFish.update_ip(start_index, end_index)
+IdleFish.update_hosts(start_index, end_index, 'C13')
+IdleFish.record(start_index, end_index)
