bb7717f1272ae723372c238d03e2d173e9999e0f
C1 <zj175@139.com>
2023-06-13 11:23:03 +0800

    新增设备001011001

--- scrcpy/001sd/001011001sd_淮荣9_闫明星.bat
+++ scrcpy/001sd/001011001sd_淮荣9_闫明星.bat
@@ -1,4 +1,5 @@
 echo 应用升级：20220708
 echo 淘宝消息：20220503
 echo 拼多多消息：20220503
-scrcpy -s 5JP0217624012161 --window-title 001021001sd_淮荣9_闫明星|
+scrcpy -s 5JP0217624012161 --window-title 001011001sd_淮荣9_闫明星|
+pause
--- scrcpy/003if/003002001if_代付_淮荣8青.bat
+++ scrcpy/003if/003002001if_代付_淮荣8青.bat
@@ -2,3 +2,4 @@ echo 应用升级：20220917
 echo 淘宝消息：20220917
 echo 拼多多消息：20220503
 scrcpy -s SQR7N17324002945 --window-title 003002001if_代付_淮荣8青|
+pause
